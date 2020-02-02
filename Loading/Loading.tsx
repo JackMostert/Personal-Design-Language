bfd4c79e664bae6ec87cf834ea6fa4de0f0acfbc
Jack Mostert <j_mostert@outlook.com>
2020-02-02 23:07:48 +0000

    updates

--- Loading/Loading.tsx
+++ Loading/Loading.tsx
@@ -17,8 +17,8 @@ export function Loading(props: ILoadingProps) {
       <div
         className={spinner && "spinner"}
         style={{
-          width: spinner.size,
-          height: spinner.size,
+          width: spinner.size || 50,
+          height: spinner.size || 50,
           border: `2px solid ${spinner.baseColor || "#b9b9b9"}`,
           borderTop: `4px solid ${spinner.accentColor || "#4c8bff"}`
         }}
