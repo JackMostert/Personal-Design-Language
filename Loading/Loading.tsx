3e82deaf0771d11c26238c734328680d05680f48
Jack Mostert <j_mostert@outlook.com>
2020-02-02 14:46:14 +0000

    saving

--- Loading/Loading.tsx
+++ Loading/Loading.tsx
@@ -1,8 +1,26 @@
 import * as React from "react";
 import "./Style.scss";
 
-export interface ILoadingProps {}
+export interface ILoadingProps {
+  style: "spinner";
+  size: string;
+  baseColor?: string;
+  accentColor?: string;
+}
 
 export function Loading(props: ILoadingProps) {
-  return <div className="Loader-root"></div>;
+  const { style, size, baseColor, accentColor } = props;
+  return (
+    <div className="Loader-root">
+      <div
+        className={style}
+        style={{
+          width: size,
+          height: size,
+          border: `2px solid ${baseColor || "#b9b9b9"}`,
+          borderTop: `4px solid ${accentColor || "#4c8bff"}`
+        }}
+      ></div>
+    </div>
+  );
 }
