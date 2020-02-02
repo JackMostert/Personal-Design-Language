550a46c7925c687eac765cd3683bdfa07388e84b
Jack Mostert <j_mostert@outlook.com>
2020-02-02 14:32:00 +0000

    saving

---
+++ Loading/Loading.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./Style.scss";
+
+export interface ILoadingProps {}
+
+export function Loading(props: ILoadingProps) {
+  return <div className="Loader-root"></div>;
+}
