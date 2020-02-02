7ffaea4e3f5635fd8dc29d955dbfc58031752ce3
Jack Mostert <j_mostert@outlook.com>
2020-02-02 02:22:53 +0000

    saving

---
+++ CardFooter/CardFooter.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { ICardFooterProps } from "./interface";
+import "./Style.scss";
+
+export default class CardFooter extends React.Component<ICardFooterProps> {
+  public render() {
+    return <div>{this.props.children}</div>;
+  }
+}
