6d996dc3622bb83d61878189478f56051eacbeef
Jack Mostert <j_mostert@outlook.com>
2020-02-02 02:28:40 +0000

    saving

--- CardFooter/CardFooter.tsx
+++ CardFooter/CardFooter.tsx
@@ -4,6 +4,6 @@ import "./Style.scss";
 
 export class CardFooter extends React.Component<ICardFooterProps> {
   public render() {
-    return <div>{this.props.children}</div>;
+    return <div className="CardFooter-root">{this.props.children}</div>;
   }
 }
